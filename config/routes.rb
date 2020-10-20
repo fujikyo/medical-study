727357513a0a1aa29eadaa4e491d8c5f6f553e70
fujikyo <guitar0324@gmail.com>
2020-10-20 15:45:23 +0900

    rooting(仮)

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'posts#index'
+  resources :posts, only:[:index,:create,:destroy,:edit] do
+    resources :comments, only:[:index,:create]
+  end
 end
