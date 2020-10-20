901ff55a87fa777229b38d4edd61290cc10de8a7
fujikyo <guitar0324@gmail.com>
2020-10-20 20:12:56 +0900

    post 投稿機能

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,4 +6,15 @@ class PostsController < ApplicationController
   def new
     @post = Post.new
   end
+
+  def create
+    Post.create(post_params)
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title,:detail).merge(user_id: current_user.id)
+  end
+
 end
